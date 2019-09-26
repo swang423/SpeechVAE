6cecc489d713ef549f91d89bab717f17d6831449
swang423 <swang423@gatech.edu>
2019-09-26 09:59:00 -0400

    my path

--- path.sh
+++ path.sh
@@ -1,14 +1,18 @@
-. /data/sls/scratch/wnhsu/env/cuda.sh
-. /usr/users/wnhsu/vtenvs/tf1.0/bin/activate 
-
-export PYTHONPATH=/data/sls/scratch/wnhsu/tmp/SpeechVAE/src:$PYTHONPATH
-export PYTHONPATH=/data/sls/scratch/wnhsu/tmp/SpeechVAE/kaldi-python:$PYTHONPATH
-
-export KALDI_ROOT=/data/sls/scratch/wnhsu/tmp/SpeechVAE/kaldi
-[ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
-export PATH=$KALDI_ROOT/tools/openfst/bin:$PATH
-export PATH=$KALDI_ROOT/egs/wsj/s5/utils:$PATH
-export PATH=$KALDI_ROOT/egs/wsj:$PATH
-[ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present"
-. $KALDI_ROOT/tools/config/common_path.sh
+case $(hostname) in 
+    "cw1"|"cw01"|"cw2"|"cw02"|"cw3"|"cw03"|"cw4"|"cw04")
+        export KALDI_ROOT=/home/swang423/kaldi18
+        [ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
+        export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/irstlm/bin/:$PWD:$PATH
+        [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
+        . $KALDI_ROOT/tools/config/common_path.sh
+        ;;
+    "pc2"|"pc02"|"pc1"|"pc01")
+        echo "On $(hostname)"
+        export KALDI_ROOT=/home/swang423/kaldi17/
+        src=src
+        export PATH=$PWD/utils/:$KALDI_ROOT/$src/bin:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/$src/fstbin/:$KALDI_ROOT/$src/gmmbin/:$KALDI_ROOT/$src/featbin/:$KALDI_ROOT/$src/lm/:$KALDI_ROOT/$src/sgmmbin/:$KALDI_ROOT/$src/sgmm2bin/:$KALDI_ROOT/$src/fgmmbin/:$KALDI_ROOT/$src/latbin/:$KALDI_ROOT/$src/nnetbin:$KALDI_ROOT/$src/nnet2bin/:$KALDI_ROOT/$src/kwsbin:$KALDI_ROOT/$src/smapbin/:$PWD:$PATH
+        ;;
+    *)
+        echo "Kaldi not compiled" && exit 1
+esac
 export LC_ALL=C
